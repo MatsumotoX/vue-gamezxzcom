f5189b71c4a6f163aa6eb740707da5ca966529e9
game <game@gamePC>
2018-06-07 19:46:46 +0700

    Add Firebase Authen

--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import * as firebase from "firebase/app";
+import "firebase/database";
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: "AIzaSyCH6dlJsTPHCGIWrnLadtSo4aUq-YbsUwA",
+  authDomain: "gamezxz-c2be1.firebaseapp.com",
+  databaseURL: "https://gamezxz-c2be1.firebaseio.com",
+  projectId: "gamezxz-c2be1",
+  storageBucket: "gamezxz-c2be1.appspot.com",
+  messagingSenderId: "1099416148640"
+};
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
